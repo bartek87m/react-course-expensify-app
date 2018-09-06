e829bee9f1a9eb72491025d90035f23240afddca
bar <bartek87m@gmail.com>
2018-09-06 20:31:23 +0200

    Removed dumm expense data

--- src/app.js
+++ src/app.js
@@ -13,16 +13,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water Bill', amount: 4400, createdAd: -23000}));
-store.dispatch(addExpense({description: 'Gas Bill', amount: 1000, createdAd: 21000}));
-store.dispatch(addExpense({description: 'Rent Bill', amount: 195000, createdAd: 1000}));
-
-const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
-
-console.log(store);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter/>
