f4b5e4e31312ad0ce4508d1bd2a7e5e1645b4933
bar <bartek87m@gmail.com>
2018-09-07 22:41:06 +0200

    Setup production build and server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.eventNames.PORT || 3000; //zmienna, którą potrzebuje heroku zeby wystartować server
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, "index.html"))
+    //spełnioa wszystkie zależności, na każdy nieznany adres przekazuje index.thml
+    //co powala używań routingu Reacta
+});
+
+app.listen(port, () => {
+    console.log("Server is up");
+});
